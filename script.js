348c7cd5afb9a5514283a621d35543d118b2d93f
MediocreCreator <franscoiswhk@gmail.com>
2024-08-10 21:07:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,241 @@
+const productsData = {
+    'Additives': {
+        'PPG': {
+            'D8430 Blender UHS Spot 1L': { image: "Images/Additives/8430.png", description: "9124408430", link: "Data/8430.pdf" },
+            'D8740 Primer Additive Plastic 1L': { image: "Images/Additives/8740.png", description: "9124408740", link: "https://za.ppgrefinish.com/en/news/2015/10/d8740-plastic-primer-additive/#" },
+            'D8750 Additive SB Plastic 1L': { image: "Images/Additives/8750.png", description: "9124408750 ", link: "" },
+            'D899 Anti Silicone 1L': { image: "Images/Additives/D899.png", description: "9124415817", link: "https://me.ppgrefinish.com/en/products/collision/ancillaries/deltron-grs-antisilicone-d899/" },
+            'D818 Accelerator 250ml': { image: "Images/Additives/D818.png", description: "9124415818", link: "Data/D818.pdf" },
+            'WPS100 Moonwipes': { image: "Images/Additives/moonwipes.png", description: "9124100030", link: "https://master.moonwalkrefinish.com/en/home/moonwipes/" },
+        },
+    },
+
+    'Clearcoats': {
+        'PPG': {
+            'D8125 Clear Coat HS 5L': { image: "Images/Clears/8125.png", description: "9124408125", link: "Data/8125.pdf"},
+            'D8177 Clear Coat Rapid Performance 5L': { image: "Images/Clears/8177.png", description: "9124408117", link: "Data/8177.pdf"},
+            'D8135 Clear Coat UHS Rapid 5L': { image: "Images/Clears/8135.png", description: "9124408240", link: "Data/8135.pdf"},
+            'D8115 Clear Coat Matt 1L': { image: "Images/Clears/8115 & 8117.png", description: "9124328113", link: "Data/8115 8117.pdf"},
+            'D8117 Clear Coat Semi Gloss 1L': { image: "Images/Clears/8115 & 8117.png", description: "9124328117", link: "Data/8115 8117.pdf"},
+            'D893 Clear VOC Performance Coat 5L': { image: "Images/Clears/893.png", description: " 9124208893", link: "Data/D893.pdf"},
+            'D800 Clear Coat 5L': { image: "Images/Clears/D800.png", description: "9124325800", link: "Data/800.pdf"},
+            'D8122 Clear Cerami 5L': { image: "Images/Clears/D8122.png", description: "9124328122", link: "Data/8122.pdf" },
+        },
+        
+        'Nexa Autocolor': {
+            "P190-6840 Clear Coat 2K Ultra 1L": {image: "Images/Clears/Nexa Ultra Clear.png", description: "9124446840", link: "Data/Ultra Clear.pdf" },
+        },
+        
+        'Max Meyer': {
+            '0200 Clear Coat HS 1L': { image: "Images/Clears/0200.png", description: "9124802001", link: "Data/0200.pdf" },
+        }
+        
+    },
+
+    'Gladiator': {
+        'Tintable':{
+            'Gladiator Kit Tintable 1L': { image: "Images/Gladiator/Gladiator Tint 1L.png", description: "9124991001", link: "Data/Gladiator.pdf" },
+            'Gladiator Kit Tintable 4L': { image: "Images/Gladiator/Gladiator Tint 4L.png", description: "9124991002", link: "Data/Gladiator.pdf" }
+        },
+
+        'Black': {
+            'Gladiator Kit Black 1L': { image: "Images/Gladiator/Gladiator Black 1L.png", description: "9124991003", link: "Data/Gladiator.pdf" },
+            'Gladiator Kit Black 4L': { image: "Images/Gladiator/Gladiator Black 4L.png", description: "9124991004", link: "Data/Gladiator.pdf" }
+        },
+
+        'Gun': {
+            'Spray Gun Gladiator': { image: "Images/Gladiator/Gladiator Gun.png", description: "9124991005", link: "Data/Gladiator.pdf" }
+        }
+    },
+
+    'Hardeners':{
+        'PPG': {
+            'D8262 Hardener HS Fast 2.5L': { image: "Images/Hardeners/8262.png", description: "9124408262", link: "" },
+            'D8263 Hardener HS Medium 2.5L': { image: "Images/Hardeners/8263.png", description: "9124408263", link: "" },
+            'D8302 Hardener UHS 0.5L': { image: "Images/Hardeners/8302.png", description: "9124408242 a versatile product used in automotive refinishing. It’s part of the PPG Deltron UHS (Ultra High Solids) compact system and is known for its efficiency in various applications", link: "" },
+            'D8238 Hardener HS Fast 1L': { image: "Images/Hardeners/8238.png", description: "9124408238", link: "" },
+            'D8305 Hardener UHS 1L': { image: "Images/Hardeners/8305.png", description: "9124408305", link: "" },
+            'D8305 Hardener UHS 2.5L': { image: "Images/Hardeners/8305.png", description: "9124408306", link: "" },
+            'D8270 Hardener HS 1L': { image: "", description: "9124408270", link: "" },
+            'D803 Hardener MS Fast 1L': { image: "Images/Hardeners/803.png", description: "9124305803", link: "" },
+            'D803 Hardener MS Fast 3L': { image: "Images/Hardeners/803 3L.png", description: "9124315803", link: "" },
+            'D8307 Hardener UHS 2.5L': { image: "Images/Hardeners/8307.png", description: "9124408307", link: "" },
+            'D8308 Hardener UHS 2.5L': { image: "Images/Hardeners/8308.png", description: "9124408308", link: "" },
+            'D841 Hardener MS Medium 1L': { image: "Images/Hardeners/841.png", description: "9124305841", link: "" },
+            'D861 Hardener MS Slow 1L': { image: "Images/Hardeners/861.png", description: "9124305861", link: "" }
+        },
+
+        'NEXA Autocolor': {
+            'P210-832 Hardener 2K MS Express 1L': { image: "Images/Hardeners/Nexa Express Hard.png", description: "9124478321", link: "" },
+            'P210-796 Hardener 2K MS Normal 0.5L': { image: "Images/Hardeners/Nexa MS Hard.png", description: "9124477960", link: "" },
+            'P210-796 Hardener 2K MS Normal': { image: "Images/Hardeners/Nexa MS Hard 1L.png", description: "9124477961", link: "" },
+            'P210-798 Hardener 2K MS Slow': { image: "Images/Hardeners/Slow Hardener.png", description: "9124477981", link: "" }
+        }
+    },
+
+    'Overalls': {
+        'PPG': {
+            "Overall Spraypainter Grey (M)": { image: "Images/Spray Suits/Overall Complete.png", description: "9124100020", link: "" },
+            "Overall Spraypainter Grey (L)": { image: "Images/Spray Suits/Overall Complete.png", description: "9124100021", link: "" },
+            "Overall Spraypainter Grey (XL)": { image: "Images/Spray Suits/Overall Complete.png", description: "9124100022", link: "" },
+            "Dustcoat Spraypainter (L)": { image: "Images/Spray Suits/Dust Coat.png", description: "9124100024", link: "" }
+        }
+    },
+
+    'Powders': {
+        'PPG': {
+            "PRLX1 Crystal Red": { image: "Images/Powders/PRLX1.png", description: "9124420001", link: "" },
+            "PRLX2 Crystal Silver": { image: "Images/Powders/PRLX2.png", description: "9124420002", link: "" },
+            "PRLX6 Solaris Red": { image: "Images/Powders/PRLX6.png", description: "9124420006", link: "" }
+        }
+    },
+
+    'Primers': {
+        'PPG': {
+            "D8501 Primer 2K White 1L": { image: "Images/Primers/8501.png", description: "9124408501", link: "Data/8501 8505 8507.pdf" },
+            "D8505 Primer 2K Grey G5 1L": { image: "Images/Primers/8505.png", description: "9124408505", link: "Data/8501 8505 8507.pdf" },
+            "D8507 Primer 2K Dark Grey G7 1L": { image: "Images/Primers/8507.png", description: "9124408507", link: "Data/8501 8505 8507.pdf" },
+            "D8541 Primer Rapid Build White G1 2L": { image: "Images/Primers/8541.png", description: "9124408541", link: "Data/8541 8545 8547.pdf" },
+            "D8545 Primer Rapid Build Grey G5 2L": { image: "Images/Primers/8545.png", description: "9124408545", link: "Data/8541 8545 8547.pdf" },
+            "D8547 Primer Rapid Build Dark Grey G7 2L": { image: "Images/Primers/8547.png", description: "9124408547", link: "Data/8541 8545 8547.pdf" },
+            "D820 Primer Adhesion Promoter Plastic 1L": { image: "Images/Primers/820.png", description: "9124400820", link: "Data/820.pdf" },
+            "D8561 Primer DP7000 G1 3L": { image: "Images/Primers/8561.png", description: "9124408561", link: "" },
+            "D8565 Primer DP7000 G5 3L": { image: "Images/Primers/8565.png", description: "9124408565", link: "" },
+            "D8567 Primer DP7000 G7 3L": { image: "Images/Primers/8567.png", description: "9124408567", link: "" }
+        },
+
+        'Nexa Autocolor': {
+            "P565-5501 Primer 2K White 2L": { image: "Images/Primers/SG 01 2L.png", description: "9124455012", link: "Data/Nexa Primer.pdf" },
+            "P565-5505 Primer 2K Grey 1L": { image: "Images/Primers/SG 05 1L.png", description: "9124455051", link: "Data/Nexa Primer.pdf" },
+            "P565-5505 Primer 2K Grey 2L": { image: "Images/Primers/SG 05 2L.png", description: "9124455052", link: "Data/Nexa Primer.pdf" },
+            "P565-5507 Primer 2K D/Grey 1L": { image: "Images/Primers/SG 07 1L.png", description: "9124455071", link: "Data/Nexa Primer.pdf" },
+            "P565-5507 Primer 2K D/Grey 2L": { image: "Images/Primers/SG 07 2L.png", description: "9124455072", link: "Data/Nexa Primer.pdf" },
+            "P565-2975 Primer Express Grey": { image: "Images/Primers/2975.png", description: "9124469751", link: "" },
+            "P565-2977 Primer Express SG7 D/Grey 1L": { image: "", description: "9124469771", link: "" },
+            "P565-2977 Primer Express SG7 D/Grey 3L": { image: "Images/Primers/2977.png", description: "9124469773", link: "" },
+            "P565-9868 Primer Etch Chromate Free Wash": { image: "Images/Primers/CFE Primer.png", description: "9124469868", link: "Data/CFE Primer.pdf" },
+            "P275-9069 Activator For CFE Primer 1L": { image: "Images/Primers/Activator.png", description: "9124469069", link: "Data/CFE Primer.pdf" }
+        }
+    },
+    
+    'PREPSOL/Spirits Wipe':{
+        'PPG': {
+        'D837 Spirit Wipe Import 5L': { image: "Images/Prepsol/D837 IMP.png", description: "9124406837", link: "Data/837.pdf" },
+        },
+        
+        'Max Meyer': {
+            'M445 Spirit Wipe MM445 5L': { image: "Images/Prepsol/M445.png", description: "9124944505", link: "" },
+        },
+        
+        'DUCOL': {
+            'D837 Spirit Wipe 5L Local': { image: "Images/Prepsol/837 Local.png", description: "9124305837", link: "" },
+        }
+    },
+
+    'Rapid Match & Digimatch': {
+        'Rapid Match': {
+            'Rapid Match Go': { image: "Images/Rapid Match/RapidGo.png", description: "9124107620", link: "https://uk.ppgrefinish.com/en/colour/colour-tools/rapidmatch-go/#" }
+        },
+
+        'Digimatch & Touch Mix': {
+            'Digimatch': { image: "Images/Rapid Match/DigiMatch.png", description: "9124107624", link: "https://www.ppg.com/refinish/en-US/color/color-devices/spectrophotometers/ppg-digimatch" },
+            'Touchmix Ultra': { image: "Images/Rapid Match/TouchMix.png", description: "9124107625", link: "https://ceb.ppgrefinish.com/en/colour/colour-tools/touchmix-pro/" }
+        }
+    },
+
+    'Thinners': {
+        'PPG/Deltron': {
+            'DU807 Thinners Medium 5L': { image: "Images/Prepsol/807 Local.png", description: "9124315807"},
+            'D807 Thinners Medium 5L': { image: "Images/Thinners/807.png", description: "9124315809"},
+            'D812 Thinners Slow 5L': { image: "Images/Thinners/812.png", description: "9124315812"},
+            'D8714 Thinners UHS Accelerated 2.5L': { image: "Images/Thinners/8714.png", description: "9124408244"},
+            'D8715 Thinners Productive 2.5L': { image: "Images/Thinners/8715.png", description: "9124408245"},
+            'D8727 Thinners DP7000 1L': { image: "Images/Thinners/8727.png", description: "9124408727"},
+            'D8728 Thinners DP7000 1L': { image: "Images/Thinners/8728.png", description: "9124408728"},
+            'D8757 Thinners Accelerator For Clear Coat 2.5L': { image: "Images/Thinners/8757.png", description: "9124408757"},
+            'D8758 Thinners Accelerator For Clear Coat 2.5L': { image: "Images/Thinners/8758.png", description: "9124408758"},
+            'D8745 Thinners Accelerator 1L': { image: "Images/Thinners/8745.png", description: "9124408745"},
+            'D8746 Thinners Accelerator High Temperature 1L': { image: "Images/Thinners/8746.png", description: "9124408746"},
+            'D868 Thinners Blending Thinners 5L': { image: "Images/Thinners/868.png", description: "9124315868"},
+          },
+    
+        'Nexa Autocolor': {
+            'P850-1491 Thinners 2K Fast 1L': { image: "Images/Thinners/Nexa Fast Thinners.png", description: "9124481491", link: ""},
+            'P850-1492 Thinners 2K Medium 1L': { image: "Images/Thinners/Nexa Medium Thinners.png", description: "9124481492", link: ""},
+            'P850-1493 Thinners 2K Slow 1L': { image: "Images/Thinners/Nexa Slow Thinners.png", description: "9124481493", link: ""}
+          }
+    },
+};
+
+window.onload = function() {
+    const selectProduct = document.getElementById('product');
+    const selectBrand = document.getElementById('brand');
+    const selectVariation = document.getElementById('variation');
+    const variationDetails = document.getElementById('variationDetails');
+    const variationImage = document.getElementById('variationImage');
+    const variationDescription = document.getElementById('variationDescription');
+    const variationLink = document.getElementById('variationLink');
+    
+    // Populate products dropdown
+    for (let product in productsData) {
+        selectProduct.options[selectProduct.options.length] = new Option(product, product);
+    }
+    
+    selectProduct.onchange = function() {
+        const selectedProduct = this.value;
+        const brands = productsData[selectedProduct];
+        
+        selectBrand.innerHTML = '<option value="" disabled selected>--Select Brand--</option>';
+        selectVariation.innerHTML = '<option value="" disabled selected>--Select Variation--</option>';
+        
+        if (selectedProduct) {
+            for (let brand in brands) {
+                selectBrand.options[selectBrand.options.length] = new Option(brand, brand);
+            }
+            
+            selectBrand.disabled = false;
+        } else {
+            selectBrand.disabled = true;
+            selectVariation.disabled = true;
+            variationDetails.style.display = 'none';
+        }
+        
+        selectVariation.disabled = true;
+        variationDetails.style.display = 'none';
+    };
+    
+    selectBrand.onchange = function() {
+        const selectedProduct = selectProduct.value;
+        const selectedBrand = this.value;
+        const variations = productsData[selectedProduct][selectedBrand];
+        
+        selectVariation.innerHTML = '<option value="" disabled selected>--Select Variation--</option>';
+        
+        if (selectedBrand) {
+            for (let variation in variations) {
+                selectVariation.options[selectVariation.options.length] = new Option(variation, variation);
+            }
+            
+            selectVariation.disabled = false;
+        } else {
+            selectVariation.disabled = true;
+            variationDetails.style.display = 'none';
+        }
+    };
+    
+    selectVariation.onchange = function() {
+        const selectedProduct = selectProduct.value;
+        const selectedBrand = selectBrand.value;
+        const selectedVariation = this.value;
+        const variationInfo = productsData[selectedProduct][selectedBrand][selectedVariation];
+        
+        if (variationInfo) {
+            variationImage.src = variationInfo.image;
+            variationDescription.textContent = variationInfo.description;
+            variationLink.href = variationInfo.link;
+            variationDetails.style.display = 'block';
+        } else {
+            variationDetails.style.display = 'none';
+        }
+    };
+};
